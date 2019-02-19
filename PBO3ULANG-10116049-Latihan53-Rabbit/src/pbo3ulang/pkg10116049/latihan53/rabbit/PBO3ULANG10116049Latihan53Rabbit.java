6bc9b922cb613eac28eb63b9934805fc5aeba11c
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 20:52:09 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan53-Rabbit/src/pbo3ulang/pkg10116049/latihan53/rabbit/PBO3ULANG10116049Latihan53Rabbit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan53.rabbit;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO3ULANG10116049Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+        
+    }
+    
+}
